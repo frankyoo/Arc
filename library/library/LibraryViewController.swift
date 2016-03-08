72856f136e998e0d335648e49602fe302cfb12d9
Marc Haumann <marchaumann@gmail.com>
2016-03-07 17:42:00 -0800

    adding library xcode project

---
+++ library/library/LibraryViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LibraryViewController.swift
+//  library
+//
+//  Created by Marc Haumann on 3/7/16.
+//  Copyright © 2016 Marc Haumann. All rights reserved.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+
+    var projects: NSArray! = ["row 1","row 2","row 3","row 4"]
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel.text = "This is row \(indexPath.row)"
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
