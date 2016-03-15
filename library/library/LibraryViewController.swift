65b8d825804e8e9b4494a74d3ec3642366fabc83
Marc Haumann <marchaumann@gmail.com>
2016-03-14 18:11:38 -0700

    testing array for storing images

--- library/library/LibraryViewController.swift
+++ library/library/LibraryViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class LibraryViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
-    var projects: NSArray! = ["row 1","row 2","row 3","row 4"]
+    var projects: NSArray! = [
+        [
+            "title" : "row 1",
+            "background" : "test_image.png"
+        
+        ],[
+            "title" : "row 2",
+            "background" : "test_image.png"
+        ]
+    ]
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = 320
         // Do any additional setup after loading the view.
     }
 
@@ -26,12 +36,18 @@ class LibraryViewController: UIViewController,UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 2
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = UITableViewCell()
-        cell.textLabel.text = "This is row \(indexPath.row)"
+        var cell = tableView.dequeueReusableCellWithIdentifier("ProjectCell") as! ProjectTableViewCell
+//        cell.textLabel!.text = "This is \(indexPath.row)"
+        var test = projects[indexPath.row]["background"]
+        print(test)
+        cell.imageView!.image = UIImage(named: "\(projects[indexPath.row]["background"])")
+//        cell.backgroundColor = UIColor(red: 230/255, green: 240/255, blue: 255/255, alpha: 1)
+        
+        // Configure YourCustomCell using the outlets that you've defined.
         
         return cell
     }
