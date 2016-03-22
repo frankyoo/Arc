d24c5f80225f88608e0892e44bd90531d839c47a
vickiheart <vickiching@gmail.com>
2016-03-22 10:35:06 -0700

    added shake to undo

--- Arc Camera/Arc Camera/ViewController.swift
+++ Arc Camera/Arc Camera/ViewController.swift
@@ -22,15 +22,21 @@ class ViewController: UIViewController, PBJVisionDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        blurEffectView.frame = CGRect(x: (view.frame.width / 2 ) - 50, y: 550, width: 100, height: 100)
+//        blurEffectView.frame = CGRect(x: (view.frame.width / 2 ) - 50, y: 550, width: 100, height: 100)
+//        shadowView.frame = blurEffectView.frame
         blurEffectView.layer.cornerRadius = 50
         blurEffectView.clipsToBounds = true
         blurEffectView.layer.borderColor = UIColor.grayColor().CGColor
         blurEffectView.layer.borderWidth = 0.5
-        blurEffectView.layer.shadowColor = UIColor.blackColor().CGColor
-        blurEffectView.layer.shadowOffset = CGSizeMake(5, 5)
         
+        blurEffectView.layer.shadowColor = UIColor.orangeColor().CGColor
+        blurEffectView.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        blurEffectView.layer.shadowOpacity = 1
+        blurEffectView.layer.shadowRadius = 1
+//        blurEffectView.layer.shadowPath = blurEffectView.path
+//        layer.insertSublayer(blurEffectView, atIndex: 0)
+        
+        view.bringSubviewToFront(blurEffectView)
         
         vision.delegate = self
         vision.cameraMode = PBJCameraMode.Photo
@@ -53,16 +59,30 @@ class ViewController: UIViewController, PBJVisionDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onUndoTap(sender: AnyObject) {
-        if photoImages.count >= 2 {
-            let priorPhoto = photoImages[photoImages.count - 2]
-            thumbnailView.image = priorPhoto
-            photoImages.removeAtIndex(photoImages.count - 1)
-        } else {
-            photoImages.removeAll()
-            thumbnailView.image = nil
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake && photoImages.count >= 1 {
+        print("started shaking")
+        
+        let alertController = UIAlertController(title: nil, message: "Undo last photo?", preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: { (UIAlertAction) -> Void in
+        })
+        alertController.addAction(cancelAction)
+        let undoAction = UIAlertAction(title: "Undo", style: .Default, handler: { (UIAlertAction) -> Void in
+            if self.photoImages.count >= 2 {
+                let priorPhoto = self.photoImages[self.photoImages.count - 2]
+                self.thumbnailView.image = priorPhoto
+                self.photoImages.removeAtIndex(self.photoImages.count - 1)
+            } else {
+                self.photoImages.removeAll()
+                self.thumbnailView.image = nil
+            }
+        })
+        alertController.addAction(undoAction)
+        presentViewController(alertController, animated: true, completion: { () -> Void in
+        })
         }
     }
+    
     
     @IBAction func onCaptureButton(sender: AnyObject) {
         vision.capturePhoto()
